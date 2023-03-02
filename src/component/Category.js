17a1c3fafa54fc236d4ae85a9e5b09c91515d90e
rkddusen <k99endus@naver.com>
2023-03-02 13:25:51 +0900

    plus Form component

--- src/component/Category.js
+++ src/component/Category.js
@@ -1,12 +1,17 @@
+import { useState } from 'react';
 import Form from './Form';
 
 function Category(props) {
+  const { category, todo } = props;
+  const [isAdd, setIsAdd] = useState(false);
+
   return (
     <div className="category">
         <div></div>
-        <p>{props.category}</p>
-        <p>+</p>
-        <p>{props.todo}</p>
+        <p>{category}</p>
+        <p onClick={() => {setIsAdd(true)}}>+</p>
+        <p>{todo}</p>
+        {isAdd ? <Form></Form> : null}
     </div>
   );
 }
