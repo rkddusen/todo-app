5e524628c4762b967ee21606e2a06a0335fa3d41
rkddusen <k99endus@naver.com>
2023-03-11 22:40:22 +0900

    edit setting component

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -16,11 +16,10 @@ function Nav(props) {
       >
         <StyledSvg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#666666" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="3"></circle><path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z"></path></StyledSvg>
       </NavSetting>
-      {isOpen ? <Setting category={category} setCategory={setCategory} todo={todo} setTodo={setTodo}></Setting> : null}
+      {isOpen ? <Setting category={category} setCategory={setCategory} todo={todo} setTodo={setTodo} setIsOpen={setIsOpen}></Setting> : null}
     </StyledNav>
   );
 }
-
 
 const StyledSvg = styled.svg`
   &:hover{
@@ -41,7 +40,6 @@ const NavSetting = styled.p`
   font-size: 30px;
   &:hover{
     cursor: pointer;
-    font-weight: bold;
   }
 `;
 
--- src/component/Setting.js
+++ src/component/Setting.js
@@ -2,26 +2,34 @@ import { useEffect, useState } from "react";
 import styled from "styled-components";
 
 function Setting(props) {
-  const { category, setCategory, todo, setTodo } = props;
+  const { category, setCategory, todo, setTodo, setIsOpen } = props;
   const [allCategory, setAllCategory] = useState([]);
+  const [formOn, setFormOn] = useState(-1);
 
   useEffect(() => {
     let _allCategory = [];
     for (let i = 0; i < category.length; i++) {
       _allCategory.push(
         <CategoryLi key={i}>
-          <Category>{category[i]}</Category>
-          <span onClick={() => DeleteCategory(i)}>×</span>
+          <Category onClick={()=>{formOn === i ? setFormOn(-1) : setFormOn(i)}}>{category[i]}</Category>
+          {formOn === i ? 
+          <CategoryForm>
+            <FormBtn onClick={() => UpdateCategory(i)}>수정</FormBtn>
+            <FormBtn onClick={() => DeleteCategory(i)}>삭제</FormBtn>
+          </CategoryForm>
+          : null}
         </CategoryLi>
       );
     }
     _allCategory.push(
       <CategoryLi key={-1}>
-        <span onClick={PlusCategory}>+</span>
+        <CategoryPlus onClick={PlusCategory}>
+        <StyledSvg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#666666" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></StyledSvg>
+        </CategoryPlus>
       </CategoryLi>
     );
     setAllCategory(_allCategory);
-  }, [category, todo]);
+  }, [category, todo, formOn]);
 
   function PlusCategory() {
     let str = "추가할 카테고리 명을 입력해주세요!";
@@ -33,12 +41,14 @@ function Setting(props) {
           else if(_new === '') _new = window.prompt("추가할 카테고리 명을 입력해주세요!");
           else if(_new !== null) {
             setCategory([...category, _new]);
+            setFormOn(-1)
             break;
           }
           else break;
         }
       } else {
         setCategory([...category, _new]);
+        setFormOn(-1)
       }
     }
   }
@@ -55,19 +65,66 @@ function Setting(props) {
         }
       }
       setTodo(_todo);
+      setFormOn(-1)
+    }
+  }
+  function UpdateCategory(index){
+    let _category = Array.from(category);
+    let _todo = Array.from(todo);
+    let _new = window.prompt("변경할 카테고리 명을 입력해주세요!");
+    if (_new !== null) { // null이 아니라면
+      if (category.indexOf(_new) !== -1 || _new === '') {
+        while(true){
+          if(category.indexOf(_new) !== -1) _new = window.prompt("중복입니다.\n변경할 카테고리 명을 입력해주세요!");
+          else if(_new === '') _new = window.prompt("변경할 카테고리 명을 입력해주세요!");
+          else if(_new !== null) {
+            _category[index] = _new;
+            setCategory(_category);
+            for(let i = 0; i < todo.length; i++){
+              if(_todo[i].category === category[index]){
+                _todo[i].category = _new;
+              }
+            }
+            setTodo(_todo);
+            setFormOn(-1)
+            break;
+          }
+          else break;
+        }
+      } else {
+        _category[index] = _new;
+            setCategory(_category);
+            for(let i = 0; i < todo.length; i++){
+              if(_todo[i].category === category[index]){
+                _todo[i].category = _new;
+              }
+            }
+            setTodo(_todo);
+            setFormOn(-1)
+      }
     }
   }
 
   return (
     <StyledSetting>
       <SettingArea>
-          <CategoryTitle>카테고리</CategoryTitle>
+        <SettingNav>
+          <SettingTitle>카테고리</SettingTitle>
+          <SettingOut onClick={()=>{setIsOpen(false)}}>
+            <StyledSvg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#666666" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></StyledSvg>
+          </SettingOut>
+        </SettingNav>
           <CategoryUl>{allCategory}</CategoryUl>
       </SettingArea>
     </StyledSetting>
   );
 }
 
+const StyledSvg = styled.svg`
+  &:hover{
+    stroke: #000000;
+  }
+`;
 const StyledSetting = styled.div`
   width: 100%;
   height: 100%;
@@ -87,11 +144,28 @@ const SettingArea = styled.div`
   box-shadow: 0px 5px 20px rgba(0, 0, 0, 0.25);
   border-radius: 20px;
   padding: 20px;
+
+  overflow-y: scroll;
+  &::-webkit-scrollbar {
+    display: none;
+  }
 `;
-const CategoryTitle = styled.div`
-  font-size: 23px;
-  font-weight: bold;
+const SettingNav = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   padding-bottom: 20px;
+`;
+const SettingTitle = styled.p`
+  font-size: 25px;
+  font-weight: bold;
+`;
+const SettingOut = styled.p`
+  font-size: 25px;
+  &:hover{
+    cursor: pointer;
+    font-weight: bold;
+  }
 `;
 const CategoryUl = styled.div`
   display: flex;
@@ -105,6 +179,34 @@ const CategoryLi = styled.div`
 const Category = styled.span`
   padding-bottom: 2px;
   border-bottom: 1px solid black;
+  font-size: 18px;
+  &:hover{
+    cursor: pointer;
+    font-weight: bold;
+  }
+`;
+const CategoryForm = styled.div`
+  margin-top: 15px;
+`;
+const FormBtn = styled.button`
+  font-size: 12px;
+  padding: 7px 30px;
+  margin: 0 10px;
+  border: none;
+  border-radius: 20px;
+  background-color: #ededed;
+
+  &:hover{
+    cursor: pointer;
+    background-color: #e0e0e0;
+  }
+`;
+const CategoryPlus = styled.span`
+  font-size: 25px;
+  &:hover{
+    cursor: pointer;
+    font-weight: bold;
+  }
 `;
 
 
